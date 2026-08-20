987e7b38485f03d1da4a0e1e7eb274934e100683
va4815 <va543567@gmail.com>
2026-08-20 21:23:53 +0100

    add spring boot liquibase project

---
+++ database/spring-boot-liquibase/src/main/java/com/va4815/springbootliquibase/SpringBootLiquibaseApplication.java
@@ -0,0 +1,13 @@
+package com.va4815.springbootliquibase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLiquibaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootLiquibaseApplication.class, args);
+    }
+
+}
---
+++ database/spring-boot-liquibase/src/test/java/com/va4815/springbootliquibase/SpringBootLiquibaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.va4815.springbootliquibase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLiquibaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
