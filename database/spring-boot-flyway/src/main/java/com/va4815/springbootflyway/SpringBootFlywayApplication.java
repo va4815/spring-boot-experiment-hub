db5298a6e23a55faf1456cf4a5e19a6419fd3c2d
va4815 <va543567@gmail.com>
2026-08-24 10:55:04 +0100

    initial spring boot flyway project

---
+++ database/spring-boot-flyway/src/main/java/com/va4815/springbootflyway/SpringBootFlywayApplication.java
@@ -0,0 +1,13 @@
+package com.va4815.springbootflyway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFlywayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootFlywayApplication.class, args);
+    }
+
+}
---
+++ database/spring-boot-flyway/src/test/java/com/va4815/springbootflyway/SpringBootFlywayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.va4815.springbootflyway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFlywayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
