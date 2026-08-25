888239eaea899d0276a88a724f098074659a442b
va4815 <va543567@gmail.com>
2026-08-25 14:16:39 +0100

    add user data in authentication module

---
+++ database/spring-boot-liquibase/src/main/resources/config/db/module/authentication/changelog/002_add_user_data.sql
@@ -0,0 +1,11 @@
+--liquibase formatted sql
+
+--changeset vincent:002
+
+INSERT INTO schema_authentication.role (name)
+VALUES ('Admin');
+
+INSERT INTO schema_authentication.user_account (role_id, username)
+    SELECT id, 'Dev Admin' FROM schema_authentication.role
+WHERE name = 'Admin';
+
