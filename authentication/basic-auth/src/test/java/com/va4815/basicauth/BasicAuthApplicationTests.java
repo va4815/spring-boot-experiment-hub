401b18d4651606f0976c284535dddc18976240a1
va4815 <va543567@gmail.com>
2026-08-31 16:37:43 +0100

    initial basic authentication project

---
+++ authentication/basic-auth/src/test/java/com/va4815/basicauth/BasicAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.va4815.basicauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
