964317d767472bff5e8bbda35f74a11456005e89
va4815 <va543567@gmail.com>
2026-09-03 10:37:28 +0100

    implemented get user by id API

--- authentication/basic-auth/src/main/java/com/va4815/basicauth/controller/UserController.java
+++ authentication/basic-auth/src/main/java/com/va4815/basicauth/controller/UserController.java
@@ -2,10 +2,7 @@ package com.va4815.basicauth.controller;
 
 import com.va4815.basicauth.entity.User;
 import com.va4815.basicauth.service.UserService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
@@ -20,6 +17,11 @@ public class UserController {
     @PostMapping
     public User createUser(@RequestBody User user) {
         return userService.createUser(user);
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Long id) {
+        return userService.getUserById(id);
     }
 
 }
--- authentication/basic-auth/src/main/java/com/va4815/basicauth/service/UserService.java
+++ authentication/basic-auth/src/main/java/com/va4815/basicauth/service/UserService.java
@@ -2,8 +2,10 @@ package com.va4815.basicauth.service;
 
 import com.va4815.basicauth.entity.User;
 import com.va4815.basicauth.repository.UserRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class UserService {
@@ -17,8 +19,33 @@ public class UserService {
     }
 
     public User createUser(User user) {
+        String username = user.getUsername();
+
+        if (userRepository.findByUsername(username).isPresent()) {
+            throw new ResponseStatusException(
+                    HttpStatus.BAD_REQUEST,
+                    "User with username: " + username + " already exists"
+            );
+        }
+
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
+    }
+
+    public User getUserById(Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(
+                        HttpStatus.NOT_FOUND,
+                        "User not found with id: " + id
+                ));
+    }
+
+    public User getUserByUsername(String username) {
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new ResponseStatusException(
+                        HttpStatus.NOT_FOUND,
+                        "User not found with username: " + username
+                ));
     }
 
 }
