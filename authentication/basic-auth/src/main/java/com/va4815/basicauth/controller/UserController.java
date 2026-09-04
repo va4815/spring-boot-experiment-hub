f381001f1b2966f87cb1eb2e091c288eb2127ded
va4815 <va543567@gmail.com>
2026-09-04 11:22:34 +0100

    implement self user info API

--- authentication/basic-auth/src/main/java/com/va4815/basicauth/controller/UserController.java
+++ authentication/basic-auth/src/main/java/com/va4815/basicauth/controller/UserController.java
@@ -1,8 +1,12 @@
 package com.va4815.basicauth.controller;
 
+import com.va4815.basicauth.config.authentication.AuthUserDetails;
 import com.va4815.basicauth.dto.UserDTO;
 import com.va4815.basicauth.entity.User;
 import com.va4815.basicauth.service.UserService;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -10,9 +14,20 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     private final UserService userService;
+    private final AuthenticationManager authenticationManager;
 
-    public UserController(UserService userService) {
+    public UserController(UserService userService, AuthenticationManager authenticationManager) {
         this.userService = userService;
+        this.authenticationManager = authenticationManager;
+    }
+
+    @GetMapping("/me")
+    public UserDTO getCurrentUser(Authentication authentication) {
+        AuthUserDetails principal = (AuthUserDetails) authentication.getPrincipal();
+
+        User user = userService.getUserByUsername(principal.getUsername());
+
+        return UserDTO.fromUser(user);
     }
 
     @PostMapping
