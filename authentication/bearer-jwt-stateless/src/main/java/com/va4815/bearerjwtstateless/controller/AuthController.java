e10c48c0ab7bf116b2fd72456a832c30e88daee9
va4815 <va543567@gmail.com>
2026-09-10 19:15:09 +0100

    add login API

--- authentication/bearer-jwt-stateless/src/main/java/com/va4815/bearerjwtstateless/controller/AuthController.java
+++ authentication/bearer-jwt-stateless/src/main/java/com/va4815/bearerjwtstateless/controller/AuthController.java
@@ -1,9 +1,34 @@
 package com.va4815.bearerjwtstateless.controller;
 
+import com.va4815.bearerjwtstateless.dto.AuthRequestDTO;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
+
+    @PostMapping("/login")
+    public ResponseEntity<Void> login(
+            @RequestBody AuthRequestDTO requestDTO,
+            HttpServletRequest request,
+            HttpServletResponse response) {
+
+        if (!StringUtils.hasText(requestDTO.username()) || !StringUtils.hasText(requestDTO.password())) {
+            throw new BadCredentialsException("Invalid username or password");
+        }
+
+        // TODO: Implement login logic
+
+
+        return ResponseEntity.ok().build();
+    }
+
 }
---
+++ authentication/bearer-jwt-stateless/src/main/java/com/va4815/bearerjwtstateless/dto/AuthRequestDTO.java
@@ -0,0 +1,4 @@
+package com.va4815.bearerjwtstateless.dto;
+
+public record AuthRequestDTO(String username, String password) {
+}
