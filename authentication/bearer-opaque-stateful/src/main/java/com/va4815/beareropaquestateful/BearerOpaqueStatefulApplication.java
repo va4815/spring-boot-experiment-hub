fc24ada13ec1513f6077a3100f236e1ad597c60e
va4815 <va543567@gmail.com>
2026-09-16 10:03:42 +0100

    initial bearer opaque stateful project

---
+++ authentication/bearer-opaque-stateful/src/main/java/com/va4815/beareropaquestateful/BearerOpaqueStatefulApplication.java
@@ -0,0 +1,13 @@
+package com.va4815.beareropaquestateful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BearerOpaqueStatefulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BearerOpaqueStatefulApplication.class, args);
+    }
+
+}
---
+++ authentication/bearer-opaque-stateful/src/test/java/com/va4815/beareropaquestateful/BearerOpaqueStatefulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.va4815.beareropaquestateful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BearerOpaqueStatefulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
