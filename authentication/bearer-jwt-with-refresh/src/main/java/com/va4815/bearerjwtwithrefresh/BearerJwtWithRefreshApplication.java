539e2d9e04f8de2bb33149ee63f38ed93d57fb4f
va4815 <va543567@gmail.com>
2026-09-19 10:37:55 +0100

    initial bearer jwt with refresh project

---
+++ authentication/bearer-jwt-with-refresh/src/main/java/com/va4815/bearerjwtwithrefresh/BearerJwtWithRefreshApplication.java
@@ -0,0 +1,13 @@
+package com.va4815.bearerjwtwithrefresh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BearerJwtWithRefreshApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BearerJwtWithRefreshApplication.class, args);
+    }
+
+}
