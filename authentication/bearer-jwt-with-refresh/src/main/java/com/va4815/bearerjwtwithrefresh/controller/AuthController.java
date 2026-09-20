2fe2e5feb26290f1926eade84329d10f35802a44
va4815 <va543567@gmail.com>
2026-09-20 11:28:41 +0100

    add user, public and auth controller

---
+++ authentication/bearer-jwt-with-refresh/src/main/java/com/va4815/bearerjwtwithrefresh/controller/AuthController.java
@@ -0,0 +1,10 @@
+package com.va4815.bearerjwtwithrefresh.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+    
+}
